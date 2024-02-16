dfd2fc6b1ba058bada697238f27db4177d1cb292
adrienromand <adrienromand@airdeadenromand.home>
2024-02-16 10:00:23 +0100

    Code complet onglet 3 désorganisé

--- Dossier Code/onglet3/onglet3.R
+++ Dossier Code/onglet3/onglet3.R
@@ -1,8 +1,12 @@
+
 # Chargement des packages nécessaires
 library(shiny)
 library(plotly)
 library(dplyr)
 library(tidyr)
+
+# Chargement des données depuis le fichier CSV en ligne
+datajo <- read.csv ("https://raw.githubusercontent.com/GabinMISARA/olympique-/main/Dossier%20Code/athlete_events.csv", sep = ";")
 
 # Chargement des données depuis le fichier CSV en ligne
 datajo <- read.csv("athlete_events.csv", sep = ";")
@@ -99,3 +103,330 @@ server <- function(input, output) {
 
 # Exécution de l'application Shiny
 shinyApp(ui = ui, server = server)
+
+
+
+####### Test Adrien #######
+
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+# git # https://github.com/GabinMISARA/olympique-
+
+library(shiny)
+library(ggplot2)
+#install.packages("shinyWidgets")
+#install.packages("plotly")
+library(shinyWidgets)
+library(plotly)
+library(dplyr)
+library (tidyr)
+
+hote <- unique(datajo$Host.country)
+# Compter le nombre de médailles pour chaque équipe
+
+medal_count <- aggregate(Medal ~ Team + Year, data=datajo, function(x) 
+  table(factor(x, levels=c("Gold", "Silver", "Bronze"))))
+
+
+datajo2 <- datajo[, -c(17, 18, 19)] 
+hote_alphab <- sort(hote)
+
+# Sélectionner les lignes de datajo2 où la valeur de la colonne "Team" est dans hote_alphab
+datajo_hote <- datajo2[datajo2$Team %in% hote_alphab, ]
+
+# Compter le nombre total de médailles remportées par année
+datajo_hote <- datajo_hote %>%
+  mutate(Medal_Gold = as.numeric(Medal == "Gold"),
+         Medal_Silver = as.numeric(Medal == "Silver"),
+         Medal_Bronze = as.numeric(Medal == "Bronze"))
+medal_count2 <- datajo_hote %>%
+  group_by(Team, Year, Season, Host.country) %>%
+  summarise(
+    Medal_Gold = sum(Medal_Gold, na.rm = TRUE),
+    Medal_Silver = sum(Medal_Silver, na.rm = TRUE),
+    Medal_Bronze = sum(Medal_Bronze, na.rm = TRUE),
+    total_Medals = sum(Medal_Gold, Medal_Silver, Medal_Bronze, na.rm = TRUE),
+    .groups = 'drop'
+  ) %>%
+  replace_na(list(Medal_Gold = 0, Medal_Silver = 0, Medal_Bronze = 0, total_Medals = 0))
+
+ui <- fluidPage(
+  
+  # Titre de l'application
+  titlePanel("Sélection de Pays"),
+  
+  # Sélection de pays
+  sidebarLayout(
+    sidebarPanel(
+      # Widget de sélection de pays
+      pickerInput(
+        inputId = "pays",
+        label = "Sélectionnez un pays :",
+        choices = c("", hote_alphab),
+        selected = NULL,
+        options = list(
+          `actions-box` = TRUE,
+          `selected-text-format` = "count > 2",
+          liveSearch = TRUE,
+          noneSelectedText = NULL
+        )
+      )
+    ),
+    mainPanel(
+      # Affichage des informations sélectionnées
+      textOutput("pays_output"),
+      plotlyOutput("hist")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  datajo <- read.csv ("https://raw.githubusercontent.com/GabinMISARA/olympique-/main/Dossier%20Code/athlete_events.csv", sep = ";")
+  #Liste des pays hôtes
+  hote <- unique(datajo$Host.country)
+  datajo2 <- datajo[, -c(17, 18, 19)] 
+  hote_alphab <- sort(hote)
+  
+  # Compter le nombre de médailles pour chaque équipe
+  medal_count <- aggregate(Medal ~ Team + Year, data=datajo, function(x) 
+    table(factor(x, levels=c("Gold", "Silver", "Bronze"))))
+  
+  # Renvoie le pays sélectionné
+  output$pays_output <- renderText({
+    if (input$pays == "") {
+      "Aucun pays sélectionné"
+    } else {
+    }
+  })
+
+  output$hist <- renderPlotly({
+    req(input$pays)
+    
+    if (!is.null(input$pays)) {
+      datajo_hote <- datajo2[datajo2$Team == input$pays, ]
+      
+      # Remplacer la création de medal_count2 par les nouvelles instructions
+      datajo_hote <- datajo_hote %>%
+        mutate(Medal_Gold = as.numeric(Medal == "Gold"),
+               Medal_Silver = as.numeric(Medal == "Silver"),
+               Medal_Bronze = as.numeric(Medal == "Bronze"))
+      
+      medal_count2 <- datajo_hote %>%
+        group_by(Team, Year, Season, Host.country, City) %>%
+        summarise(
+          Medal_Gold = sum(Medal_Gold, na.rm = TRUE),
+          Medal_Silver = sum(Medal_Silver, na.rm = TRUE),
+          Medal_Bronze = sum(Medal_Bronze, na.rm = TRUE),
+          total_Medals = sum(Medal_Gold, Medal_Silver, Medal_Bronze, na.rm = TRUE),
+          .groups = 'drop'
+        ) %>%
+        replace_na(list(Medal_Gold = 0, Medal_Silver = 0, Medal_Bronze = 0, total_Medals = 0))
+      
+      p <- plot_ly(
+        x = ~medal_count2$Year,
+        type = 'bar',
+        hoverinfo = 'text',
+        text = paste("Année: ", medal_count2$Year,
+                     "<br>Médailles d'or: ", medal_count2$Medal_Gold,
+                     "<br>Médailles d'argent: ", medal_count2$Medal_Silver,
+                     "<br>Médailles de bronze: ", medal_count2$Medal_Bronze,
+                     "<br>Pays hôte: ", input$pays,
+                     "<br>Ville: ", medal_count2$City)
+      )
+      
+      # Ajouter une seule trace pour les médailles
+      p <- p %>% add_trace(
+        y = ~medal_count2$Medal_Gold,
+        name = 'Or',
+        marker = list(color = 'gold', 
+                      line = list(color = ifelse(medal_count2$Year %in% datajo_hote$Year[datajo_hote$Host.country == input$pays], 'purple','transparent'), width = 2)
+        )) %>%
+        add_trace(
+          y = ~medal_count2$Medal_Silver,
+          name = 'Argent',
+          marker = list(color = 'silver',
+                        line = list(color = ifelse(medal_count2$Year %in% datajo_hote$Year[datajo_hote$Host.country == input$pays], 'purple','transparent'), width = 2)
+          )) %>%
+        add_trace(
+          y = ~medal_count2$Medal_Bronze,
+          name = 'Bronze',
+          marker = list(color = 'peru',
+                        line = list(color = ifelse(medal_count2$Year %in% datajo_hote$Year[datajo_hote$Host.country == input$pays], 'purple','transparent'), width = 2)
+          ))
+      
+      
+      p <- p %>%
+        layout(
+          title = paste("Nombre total de médailles pour", input$pays),
+          xaxis = list(title = "Année JO (année hôte en violet)"),
+          yaxis = list(title = "Nombre de médailles"),
+          barmode = 'stack',  # Pour empiler les barres
+          hoverlabel = list(bgcolor = 'rgb(235, 235, 235)', font = list(color = 'rgb(145, 145, 145)'))
+        )
+      }
+  })
+  
+}
+
+
+shinyApp(ui = ui, server = server)
+
+
+##### Pistes d'amélioration application
+
+### Pour l'ensemble de l'application shiny, il serait intéressant de développer davantage le design
+# de l'interface d'un point de vue esthétique (arrière-plan et thème de couleur notamment).
+
+#### créer une version démo (moins de données) et une version pleine
+
+
+##### Il existe différentes pistes d'amélioration que j'aurais aimé apporter au code de cette partie.
+
+### retirer "trace 0" du graphique qui empêche sa lisibilité
+
+### Remplir l'arrière plan du hoverlab en violet pour les années où le pays choisi était hôte mais
+# quand j'utilisais : "hoverlabel = list(bgcolor = ifelse(medal_count2$Year %in% 
+#datajo_hote$Year[datajo_hote$Host.country == input$pays], 'purple', 'rgb(235, 235, 235)'), 
+#font = list(color = 'rgb(145, 145, 145)')" cela redefinissait l'hoverlabel par rapport aux
+# couleurs des sous-parties des barres (Bronze, Argent et Or)
+
+### Ajouter les données des années manquantes dans la base de données (2018; 2020 et 2022) à partir
+#d'une autre base de données. Mais aussi résoudre les problèmes liés à la base de données qui 
+#empêchent l'affichage des données pour certains pays (United States, Yougouslavie, ???)
+
+### Pouvoir traduire le nom des pays et des villes en français lorsqu'ils 
+# sont appelés par l'utilisateur. De cette façon il n'y aurait pas de mélange de français et d'anglais
+# notamment sur l'histogramme et dans la liste déroulante.
+
+### Proposer une prédiction des résultats des JO 2024 de Paris sur la base d'un modèle de régression.
+# De cette façon il aurait pu être amusant d'estimer s'il serait probable que la France performe à Paris.
+# Ensuite, il aurait pu être intéressant d'essayer de quantifier la corrélation entre les victoires aux JO
+#et le fait d'être le pays organisateur. Cela aurait permis d'améliorer la précision de la prédiction.
+
+
+
+
+####### courbe de tendance des résultats aux JO pour cette équipe, ainsi que les prédictions pour les JO 2024.
+
+# Charger les données
+datajo <- read.csv("https://raw.githubusercontent.com/GabinMISARA/olympique-/main/Dossier%20Code/athlete_events.csv", sep = ";")
+
+
+# Tracer la tendance du nombre de médailles pour chaque équipe
+library(shiny)
+library(plotly)
+library(dplyr)
+
+# Filtrer les données pour inclure uniquement les pays ayant des données à partir de 2010
+datajo3 <- datajo %>%
+  mutate(Medal_Gold = as.numeric(Medal == "Gold"),
+         Medal_Silver = as.numeric(Medal == "Silver"),
+         Medal_Bronze = as.numeric(Medal == "Bronze")) %>%
+  group_by(Team, Year) %>%
+  summarise(
+    total_Medals = sum(Medal_Gold, Medal_Silver, Medal_Bronze, na.rm = TRUE),
+    .groups = 'drop'
+  ) %>%
+  replace_na(list(Medal_Gold = 0, Medal_Silver = 0, Medal_Bronze = 0, total_Medals = 0)) %>%
+  filter(Team %in% (filter(., Year >= 2010) %>% group_by(Team) %>% filter(sum(total_Medals) > 0) %>% pull(Team)))
+
+# Exclure la Russie (en raison de la situation militaire et géo-politique actuelle)
+datajo3 <- datajo3 %>%
+  filter(Team != "Russia")
+
+# Trier les données en fonction du nombre total de médailles
+top_countries <- datajo3 %>%
+  group_by(Team) %>%
+  summarise(total_Medals = sum(total_Medals)) %>%
+  top_n(10, total_Medals) %>%
+  pull(Team)
+
+# Filtrer les données pour inclure uniquement les 10 premiers pays
+medal_count_top <- datajo3 %>%
+  filter(Team %in% top_countries)
+
+ui <- fluidPage(
+  titlePanel("Tendance du nombre de médailles pour les 10 pays les plus performants"),
+  plotlyOutput("medal_trend_plot"),
+  textOutput("podium_ranking")
+)
+
+server <- function(input, output) {
+  # Votre code pour charger les données datajo et effectuer les calculs
+  
+  output$medal_trend_plot <- renderPlotly({
+    # Remplacer la création de medal_count2 par les nouvelles instructions
+    datajo3 <- datajo %>%
+      mutate(Medal_Gold = as.numeric(Medal == "Gold"),
+             Medal_Silver = as.numeric(Medal == "Silver"),
+             Medal_Bronze = as.numeric(Medal == "Bronze"))
+    
+    medal_count3 <- datajo3 %>%
+      group_by(Team, Year) %>%
+      summarise(
+        total_Medals = sum(Medal_Gold, Medal_Silver, Medal_Bronze, na.rm = TRUE),
+        .groups = 'drop'
+      ) %>%
+      replace_na(list(Medal_Gold = 0, Medal_Silver = 0, Medal_Bronze = 0, total_Medals = 0))
+    
+    # Filtrer les données pour inclure uniquement les pays ayant des données à partir de 2010
+    medal_count3 <- medal_count3 %>%
+      filter(Team %in% (medal_count3 %>% filter(Year >= 2010) %>% group_by(Team) %>% filter(sum(total_Medals) > 0) %>% pull(Team)))
+    
+    # Exclure la Russie (en raison de la situation militaire et géo-politique actuelle)
+    medal_count3 <- medal_count3 %>%
+      filter(Team != "Russia")
+    
+    # Trier les données en fonction du nombre total de médailles
+    top_countries <- medal_count3 %>%
+      group_by(Team) %>%
+      summarise(total_Medals = sum(total_Medals)) %>%
+      top_n(10, total_Medals) %>%
+      pull(Team)
+    
+    # Filtrer les données pour inclure uniquement les 10 premiers pays
+    medal_count_top <- medal_count3 %>%
+      filter(Team %in% top_countries)
+    
+    # Convertir les résultats en un format adapté pour la création de graphiques
+    medal_count_top <- as.data.frame(medal_count_top)
+    
+    # Convertir les années en numérique pour assurer l'ordre correct sur l'axe des x
+    medal_count_top$Year <- as.numeric(as.character(medal_count_top$Year))
+    
+    # Tracer la tendance du nombre de médailles pour chaque équipe avec Plotly
+    p <- plot_ly()
+    
+    for (team in unique(medal_count_top$Team)) {
+      team_data <- medal_count_top[medal_count_top$Team == team, ]
+      model <- lm(total_Medals ~ Year, data = team_data)
+      preds <- data.frame(Year = seq(min(team_data$Year), 2024))
+      preds$total_Medals <- predict(model, newdata = preds)
+      # Si la prédiction est négative, la remplacer par 0
+      preds$total_Medals <- ifelse(preds$total_Medals < 0, 0, preds$total_Medals)
+      p <- add_trace(p, data = preds, x = ~Year, y = ~total_Medals, type = 'scatter', mode = 'lines', name = team)
+    }
+    
+    p <- p %>%
+      layout(
+             xaxis = list(title = "Année"),
+             yaxis = list(title = "Nombre de médailles"))
+    
+    p
+  })
+
+  
+}
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
